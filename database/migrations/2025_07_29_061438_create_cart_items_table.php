6e8838e316d54e4f1ff5364ef42461bbc550e440
Alyssandria <sanchez.roniel.work@gmail.com>
2025-08-12 09:49:47 +0800

    feat: init migrations

--- database/migrations/2025_07_29_061438_create_cart_items_table.php
+++ database/migrations/2025_07_29_061438_create_cart_items_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('cart_items', function (Blueprint $table) {
             $table->id();
             $table->foreignId('cart_id')
-                ->unique()
                 ->constrained()
                 ->cascadeOnDelete()
                 ->cascadeOnUpdate();
---
+++ database/migrations/2025_08_12_011000_create_orders_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->date('date');
+            $table->float('total');
+            $table->integer('status')
+                ->default(2);
+            $table->integer('payment')
+                ->default(1);
+            $table->foreignId('contact_id')
+                ->constrained()
+                ->cascadeOnDelete();
+            $table->foreignId('shipping_id')
+                ->constrained()
+                ->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
